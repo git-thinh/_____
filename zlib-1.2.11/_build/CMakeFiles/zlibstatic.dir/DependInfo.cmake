3b3d52011c0c0205082146d7871e77261e344aef
git-thinh <33323583+git-thinh@users.noreply.github.com>
2018-01-22 12:50:17 +0700

    Initial commit

---
+++ zlib-1.2.11/_build/CMakeFiles/zlibstatic.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/_____/zlib-1.2.11/adler32.c" "C:/_____/zlib-1.2.11/_build/CMakeFiles/zlibstatic.dir/adler32.obj"
+  "C:/_____/zlib-1.2.11/compress.c" "C:/_____/zlib-1.2.11/_build/CMakeFiles/zlibstatic.dir/compress.obj"
+  "C:/_____/zlib-1.2.11/crc32.c" "C:/_____/zlib-1.2.11/_build/CMakeFiles/zlibstatic.dir/crc32.obj"
+  "C:/_____/zlib-1.2.11/deflate.c" "C:/_____/zlib-1.2.11/_build/CMakeFiles/zlibstatic.dir/deflate.obj"
+  "C:/_____/zlib-1.2.11/gzclose.c" "C:/_____/zlib-1.2.11/_build/CMakeFiles/zlibstatic.dir/gzclose.obj"
+  "C:/_____/zlib-1.2.11/gzlib.c" "C:/_____/zlib-1.2.11/_build/CMakeFiles/zlibstatic.dir/gzlib.obj"
+  "C:/_____/zlib-1.2.11/gzread.c" "C:/_____/zlib-1.2.11/_build/CMakeFiles/zlibstatic.dir/gzread.obj"
+  "C:/_____/zlib-1.2.11/gzwrite.c" "C:/_____/zlib-1.2.11/_build/CMakeFiles/zlibstatic.dir/gzwrite.obj"
+  "C:/_____/zlib-1.2.11/infback.c" "C:/_____/zlib-1.2.11/_build/CMakeFiles/zlibstatic.dir/infback.obj"
+  "C:/_____/zlib-1.2.11/inffast.c" "C:/_____/zlib-1.2.11/_build/CMakeFiles/zlibstatic.dir/inffast.obj"
+  "C:/_____/zlib-1.2.11/inflate.c" "C:/_____/zlib-1.2.11/_build/CMakeFiles/zlibstatic.dir/inflate.obj"
+  "C:/_____/zlib-1.2.11/inftrees.c" "C:/_____/zlib-1.2.11/_build/CMakeFiles/zlibstatic.dir/inftrees.obj"
+  "C:/_____/zlib-1.2.11/trees.c" "C:/_____/zlib-1.2.11/_build/CMakeFiles/zlibstatic.dir/trees.obj"
+  "C:/_____/zlib-1.2.11/uncompr.c" "C:/_____/zlib-1.2.11/_build/CMakeFiles/zlibstatic.dir/uncompr.obj"
+  "C:/_____/zlib-1.2.11/zutil.c" "C:/_____/zlib-1.2.11/_build/CMakeFiles/zlibstatic.dir/zutil.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_LARGEFILE64_SOURCE=1"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
